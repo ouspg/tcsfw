f50dd851c57f371803d6f8b932ac900c886bfd09
Rauli Kaksonen <43605561+raulikak@users.noreply.github.com>
2024-03-13 17:36:47 +0200

    Initialize Sqlalchemy

--- tcsfw/sql_database.py
+++ tcsfw/sql_database.py
@@ -1,7 +1,9 @@
-from tcsfw.entity_database import EntityDatabase
+from tcsfw.entity_database import EntityDatabase, InMemoryDatabase
+from sqlalchemy import create_engine
 
-
-class SQLDatabase(EntityDatabase):
+class SQLDatabase(InMemoryDatabase):
+    """Use SQL database for storage"""
     def __init__(self, db_uri: str):
         super().__init__()
-        
+        self.engine = create_engine(db_uri)
+        self.db_conn = self.engine.connect()
